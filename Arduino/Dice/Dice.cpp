48906ac2ef1c78d4c9ae6aff350ad449ecd3a2c2
Murithi <markkibara2014@gmail.com>
2022-09-04 23:28:19 +0300

    "Added Dice.cpp"

--- Arduino/Dice/Dice.cpp
+++ Arduino/Dice/Dice.cpp
@@ -1,5 +1,124 @@
-//C++ code
-void setup ()
-{
+// C++ code
 
+int On=1;
+int Off;
+void setup()
+{
+  pinMode(1,OUTPUT);
+  pinMode(2,OUTPUT);
+  pinMode(3,OUTPUT);
+  pinMode(4,OUTPUT);
+  pinMode(5,OUTPUT);
+  pinMode(6,OUTPUT);
+  pinMode(7,OUTPUT);
+  pinMode(A0, INPUT_PULLUP);
+}
+
+void loop()
+{
+  int buttonState = digitalRead(A0);
+  if(buttonState==LOW){
+  rolltheDice(10,100);
+  rolltheDice(5,200);
+  rolltheDice(3,300);
+  rolltheDice(1,100);
+  }
+}
+
+void rolltheDice(int count,int delayLength){
+  for (int i=0;i<count;i++){
+    int number = random(1,7);
+    showNumber(number);
+    delay(delayLength);
+  }
+}
+
+void showNumber(int x){
+  if (On==1){
+    Off=0;
+  }
+  else{
+    Off=1;
+  }
+  
+  switch(x){
+     case 1: one();   break;
+     case 2: two();   break;
+     case 3: three(); break;
+     case 4: four();  break;
+     case 5: five();  break;
+     case 6: six();   break;
+     default: zero(); break;
+  }
+}
+
+void one(){
+  digitalWrite(1, LOW);
+  digitalWrite(2, HIGH);
+  digitalWrite(3, HIGH);
+  digitalWrite(4, LOW);
+  digitalWrite(5, LOW);
+  digitalWrite(6, LOW);
+  digitalWrite(7, LOW);
+}
+
+void two(){
+  digitalWrite(1, HIGH);
+  digitalWrite(2, HIGH);
+  digitalWrite(3, LOW);
+  digitalWrite(4, HIGH);
+  digitalWrite(5, HIGH);
+  digitalWrite(6, LOW);
+  digitalWrite(7, HIGH);
+}
+
+void three(){
+  digitalWrite(1, HIGH);
+  digitalWrite(2, HIGH);
+  digitalWrite(3, HIGH);
+  digitalWrite(4, HIGH);
+  digitalWrite(5, LOW);
+  digitalWrite(6, LOW);
+  digitalWrite(7, HIGH);
+}
+
+void four(){
+  digitalWrite(1, LOW);
+  digitalWrite(2, HIGH);
+  digitalWrite(3, HIGH);
+  digitalWrite(4, LOW);
+  digitalWrite(5, LOW);
+  digitalWrite(6, HIGH);
+  digitalWrite(7, HIGH);
+
+}
+
+void five(){
+  digitalWrite(1, HIGH);
+  digitalWrite(2, LOW);
+  digitalWrite(3, HIGH);
+  digitalWrite(4, HIGH);
+  digitalWrite(5, LOW);
+  digitalWrite(6, HIGH);
+  digitalWrite(7, HIGH);
+}
+
+void six(){
+  digitalWrite(1, HIGH);
+  digitalWrite(2, LOW);
+  digitalWrite(3, HIGH);
+  digitalWrite(4, HIGH);
+  digitalWrite(5, HIGH);
+  digitalWrite(6, HIGH);
+  digitalWrite(7, HIGH);
+}
+
+void zero(){                 
+  digitalWrite(1, HIGH);
+  digitalWrite(2, HIGH);
+  digitalWrite(3, HIGH);
+  digitalWrite(4, HIGH);
+  digitalWrite(5, HIGH);
+  digitalWrite(6, HIGH);
+  digitalWrite(7, LOW);  
 }
